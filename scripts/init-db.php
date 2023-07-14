8d30e3c7f3aadd21f9e43b2eec5ed878f09f399c
Michele Orselli <michele.orselli@gmail.com>
2023-07-14 12:12:25 +0200

    wip

--- scripts/init-db.php
+++ scripts/init-db.php
@@ -2,11 +2,12 @@
 
 $user = 'postgres';
 $password = getenv('PGPASSWORD');
+$host = getenv('PGHOST');
 $port = getenv('PGPORT');
 
 $data = explode("\n", file_get_contents(__DIR__ . '/../tests/data/00-init-databases-and-users.sql'));
 
-$connection = new \PDO("pgsql:port=$port;host=postgresql-test;user=$user;password=$password");
+$connection = new \PDO("pgsql:port=$port;host=$host;user=$user;password=$password");
 
 foreach ($data as $query) {
     $connection->exec($query);
